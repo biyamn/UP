20cf928c251de00a597a2d9e8e3674872c2a025e
HamZik <b1y9mn@gmail.com>
2023-07-29 17:33:59 +0900

    feat: 레이아웃 구성
      - 상단에 풍선 이미지 추가
      - 중앙에 집 이미지 추가
      - 하단에 구름 이미지 추가
      - 배경색 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import { motion } from "framer-motion";
+import BalloonIcon from "./components/BalloonIcon";
+import houseIcon from "./assets/house_pixel.png";
+import cloudIcon from "./assets/cloud_pixel.png";
+import styled from "styled-components";
 
+// 이곳에 모두 구현 후 분리 예정
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Container>
+      <Balloons>
+        <BalloonIcon />
+        <BalloonIcon />
+        <BalloonIcon />
+      </Balloons>
+      <img src={houseIcon} alt="하우스" />
+      <img src={cloudIcon} alt="구름" style={{ width: "40%" }} />
+    </Container>
+  );
 }
 
-export default App
+const Container = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Balloons = styled(motion.div)`
+  display: flex;
+  flex-direction: row;
+`;
+
+export default App;
